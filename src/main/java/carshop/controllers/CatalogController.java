bedbcb9903a498d56a2ef6d7a35eb0b7dde2465e
MaksKrysevych <maks.krysevych@gmail.com>
2023-01-05 18:37:00 +0200

    updates

--- src/main/java/carshop/controllers/CatalogController.java
+++ src/main/java/carshop/controllers/CatalogController.java
@@ -1,7 +1,10 @@
 package carshop.controllers;
 
+import carshop.service.AdvertisementService;
 import carshop.service.CarService;
 import carshop.service.GalleryService;
+import carshop.service.RequestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,16 +12,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class CatalogController {
+    private final AdvertisementService advertisementService;
     private final CarService carService;
     private final GalleryService galleryService;
 
-    public CatalogController(CarService carService, GalleryService galleryService) {
+    @Autowired
+    public CatalogController(AdvertisementService advertisementService, CarService carService, GalleryService galleryService, RequestService requestService) {
+        this.advertisementService = advertisementService;
         this.carService = carService;
         this.galleryService = galleryService;
     }
 
     @GetMapping("/catalog")
     public String catalog(Model model){
+        model.addAttribute("adverts", advertisementService.getAllAdverts());
         model.addAttribute("cars", carService.getAllCars());
         model.addAttribute("galleries", galleryService.getAllGalleries());
 
@@ -26,9 +33,10 @@ public class CatalogController {
     }
 
     @GetMapping("/details/{id}")
-    public String details(@PathVariable(value = "id") Long car_id, Model model){
-        model.addAttribute("car", carService.getCarById(car_id));
-        model.addAttribute("gallery", galleryService.getGalleryById(car_id));
+    public String details(@PathVariable(value = "id") Long advert_id, Model model){
+        model.addAttribute("advert", advertisementService.getAdvertById(advert_id));
+        model.addAttribute("car", carService.getCarById(advertisementService.getAdvertById(advert_id).getCarId()));
+        model.addAttribute("gallery", galleryService.getGalleryById(advertisementService.getAdvertById(advert_id).getPhotoId()));
 
         return "details";
     }
